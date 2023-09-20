29b8ec8feb08fc0e3ccf3e62ffa4a1a6f12d1a65
Hallison da Paz <hallison@pgdinamica.com>
2023-09-20 16:10:52 -0300

    tested graphviz module, but it depends on graphviz software

---
+++ viz/viz.py
@@ -0,0 +1,14 @@
+import graphviz
+
+dot = graphviz.Digraph(comment='The Round Table')
+
+dot.node('A', 'King Arthur')  
+dot.node('B', 'Sir Bedevere the Wise')
+dot.node('L', 'Sir Lancelot the Brave')
+
+dot.edges(['AB', 'AL'])
+dot.edge('B', 'L', constraint='false')
+
+# doctest_mark_exe()
+
+dot.render('viz/round-table.gv', view=True)
